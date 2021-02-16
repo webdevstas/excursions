a0754811d621cf76d34a747b7816664a7ef7c16e
techhome <tech-home@mail.ru>
2021-02-16 10:08:19 +0300

    правки по безопасности, обновил версию node для контейнера

--- app.js
+++ app.js
@@ -43,6 +43,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.disable('x-powered-by');
 
 // session
 const sessionStore = new MongoStore({
