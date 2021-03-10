62d1e81b7602236f4a88c52dfade24c54cfa2d44
techhome <tech-home@mail.ru>
2021-03-10 09:49:21 +0300

    переписал роут главной на рендер после получения даннных

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,23 @@ const {countCompanies} = require('../controllers/companies')
 const {countExcursions} = require('../controllers/excursions')
 
 
-
 /* GET home page. */
-router.get('/', async function (req, res, next) {
-  let countComp = await countCompanies()
-  let countExc = await countExcursions()
-  let username = req.user ? req.user.username : 'guest'
-  res.render('index', { title: 'Главная', user: username, countCompanies: countComp, countExcursions: countExc});
+router.get('/', function (req, res, next) {
+    let username = req.user ? req.user.username : 'guest'
+  // eslint-disable-next-line no-async-promise-executor
+    let countPromise = new Promise(async (resolve, reject) => {
+        let countComp = await countCompanies()
+        let countExc = await countExcursions()
+        resolve({countComp, countExc})
+    })
+    countPromise.then((data) => {
+        res.render('index', {
+            title: 'Главная',
+            user: username,
+            countCompanies: data.countComp,
+            countExcursions: data.countExc
+        })
+    })
 });
 
 module.exports = router;
