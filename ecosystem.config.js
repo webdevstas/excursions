34bd3419b7257fffb668a55cb40f7e7d524302aa
techhome <tech-home@mail.ru>
2021-03-19 10:41:39 +0300

    изменил директорию логов и увеличил инстансы процесса пм

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,16 +2,14 @@ module.exports = {
   apps: [{
     name: 'app',
     script: './bin/www',
-    // watch: true,
-    // ignore_watch: ["node_modules", "*.log"],
     exec_mode: 'cluster',
-    instances: 2,
+    instances: 4,
     env: {
       NODE_ENV: "production",
     },
-    error_file: 'err.log',
-    out_file: 'out.log',
-    log_file: 'combined.log',
+    error_file: 'logs/err.log',
+    out_file: 'logs/out.log',
+    log_file: 'logs/combined.log',
     time: true
   }]
 };
