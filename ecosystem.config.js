6b6da299902b40216cb0e0d12c2772d52e3b4c5b
techhome <tech-home@mail.ru>
2021-03-31 10:38:05 +0300

    change ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,9 +7,9 @@ module.exports = {
     env: {
       NODE_ENV: "production",
     },
-    error_file: 'logs/err.log',
-    out_file: 'logs/out.log',
-    log_file: 'logs/combined.log',
+    error_file: './logs/err.log',
+    out_file: './logs/out.log',
+    log_file: './logs/combined.log',
     time: true
   }]
 };
